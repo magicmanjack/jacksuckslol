e8e9a6b8f66c49835dfc6062ae4b59f9eccde779
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2020-03-26 19:41:16 +1300

    Compleeeetely changed it

--- jacksfirst.py
+++ jacksfirst.py
@@ -1,34 +1,60 @@
 import pygame
+import time
+
+update_rate = 60 #Desired updates per second
 
 pygame.init()
 
-y_vel = -0.5
-y_acc = 0.001
-x = 275.0
-y = 575.0
-
 screen = pygame.display.set_mode((600, 600))
 
-def drawToScreen():
-    screen.fill((0, 0, 0))
+#def update():
+    #all game updates are done in here
 
-    global y
-    global y_vel
-    global y_acc
 
-    y += y_vel
-    if(y >= 575.0 and y_vel > 0):
-       y_vel *= -1
-    y_vel += y_acc
+#def render():
+    #all game drawing is done in here
     
-    pygame.draw.circle(screen, (255, 0, 0), (int(x), int(y)), 25)
-    pygame.display.update()
 
 running = True
+time_now = time.time_ns()
+time_then = time_now
+interval_time = 1000000000.0 / float(update_rate)
+updates_queued = 0.0
+updates = 0
+frames = 0
+can_render = False
+
+print_time = time.time()
+
 while(running):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    drawToScreen()
+
+    can_render = False
     
+    time_now = time.time_ns()
+    updates_queued += (time_now - time_then) / interval_time
+    time_then = time_now
+
+    while(updates_queued > 0):
+        updates_queued-=1
+        updates += 1
+        #update()
+        can_render = True
+
+    if(can_render):
+        screen.fill((0, 0, 0))
+        #render()
+        pygame.display.update()
+        frames += 1
+
+    if(time.time() - print_time >= 1):
+        print_time += 1
+        print("Updates:", updates, "\nFrames:", frames)
+        updates = 0
+        frames = 0
+
+    for event in pygame.event.get():
+        if(event.type == pygame.QUIT):
+            running = False
+                
+        
 pygame.quit()
