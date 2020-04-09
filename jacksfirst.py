8c2b2a3693bd67b8efccadbfd2616439295fa28a
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2020-04-09 16:12:57 +1200

    Multiplayer thingy prototype

--- jacksfirst.py
+++ jacksfirst.py
@@ -3,23 +3,49 @@ import time
 
 game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 tile_width = 200
-global white
+player_turn = 1 # player 1 always starts
 white = (255, 255, 255)
+green = (0, 255, 0)
+
+txt_box_active = False
+txt_box_active_color = (0, 150, 0)
+txt_box_color = green
+txt_box = pygame.Rect(100, 250, 400, 100)
+txt_box_delay = 2
+delay_start_time = time.time()
+
+txt_box_input = u" "
+out_code = u""
+
 
 update_rate = 60 #Desired updates per second
+
+order = int(input("Enter '1' or '2' to choose who goes first: "))
+waiting = bool(order - 1)
 
 pygame.init()
 
 screen = pygame.display.set_mode((600, 600))
 
+def turn_next():
+    global player_turn
+    global turns
+    if player_turn == 1:
+        player_turn = 2
+    elif player_turn == 2:
+        player_turn = 1
+
 def draw_cross(x, y):
+    global white
     pygame.draw.line(screen, white, (x, y), (x + tile_width, y + tile_width), 8)
     pygame.draw.line(screen, white, (x, y + tile_width), (x + tile_width, y), 8)
     
 def draw_nought(x, y):
+    global white
     pygame.draw.circle(screen, white, (x + tile_width // 2, y + tile_width // 2), tile_width // 2, 8)
 
 def draw_grid():
+    global white
     pygame.draw.line(screen, white, (200, 0), (200, 600), 8)
     pygame.draw.line(screen, white, (400, 0), (400, 600), 8)
     pygame.draw.line(screen, white, (0, 200), (600, 200), 8)
@@ -27,18 +53,66 @@ def draw_grid():
     
 def update():
     #all game updates are done in here
+    global running
     global ms_x, ms_y
     global game_board
-    ms_x, ms_y = pygame.mouse.get_pos()
-    if(pygame.mouse.get_pressed()[0]):
-        game_board[int(ms_y / tile_width)][int(ms_x / tile_width)] = 1
-    elif(pygame.mouse.get_pressed()[2]):
-        game_board[int(ms_y / tile_width)][int(ms_x / tile_width)] = 2
+    global player_turn
+    global waiting
+    global delay_start_time
+    global txt_box
+    global txt_box_active
+    global txt_box_color
+    global txt_box_active_color
+    global txt_box_input
+    global txt_box_delay
+    global green
+    global out_code
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and txt_box_active:
+            if event.key == pygame.K_RETURN:
+                coordinates = txt_box_input.split("-")
+                game_board[int(coordinates[0])][int(coordinates[1])] = player_turn
+                txt_box_input = u""
+                waiting = False
+                txt_box_active = False
+                txt_box_color = green
+                turn_next()
+            elif event.key == pygame.K_BACKSPACE:
+                txt_box_input = txt_box_input[:-1]
+            else:
+                txt_box_input += event.unicode
+            
     
+    ms_x, ms_y = pygame.mouse.get_pos()
+    
+    
+    if pygame.mouse.get_pressed()[0] and not game_board[int(ms_y / tile_width)][int(ms_x / tile_width)] and not waiting:
+        game_board[int(ms_y / tile_width)][int(ms_x / tile_width)] = player_turn
+        out_code = str(int(ms_y / tile_width)) + "-" + str(int(ms_x / tile_width))
+        turn_next()
+        waiting = True
+        delay_start_time = time.time()
+        
+    elif pygame.mouse.get_pressed()[0] and time.time() - delay_start_time >= txt_box_delay:
+        if txt_box.collidepoint(pygame.mouse.get_pos()):
+            txt_box_active = True
+            txt_box_color = txt_box_active_color
+        else:
+            txt_box_active = False
+            txt_box_color = green
 
 
 def render():
     #all game drawing is done in here
+    global txt_box
+    global txt_box_color
+    global txt_box_active_color
+    global txt_box_delay
+    global txt_box_input
+    global out_code
     
     for iy in range(0, 3):
         for ix in range(0, 3):
@@ -48,6 +122,14 @@ def render():
                 draw_nought(ix * tile_width, iy * tile_width)
 
     draw_grid()
+    
+    if waiting and time.time() - delay_start_time >= txt_box_delay:
+        pygame.draw.rect(screen, txt_box_color, txt_box)
+        line1 = pygame.font.Font(None, 32).render("Send this code to opponent: " + out_code, True, (0, 0, 0))
+        line2 = pygame.font.Font(None, 32).render("Enter opponents code: " + txt_box_input, True, (0, 0, 0))
+        screen.blit(line1, (txt_box.x + 10, txt_box.y + 10))
+        screen.blit(line2, (txt_box.x + 10, txt_box.y + 50))
+        
                 
 running = True
 time_now = time.time_ns()
@@ -85,10 +167,6 @@ while(running):
     #    print("Updates:", updates, "\nFrames:", frames)
     #    updates = 0
     #    frames = 0
-
-    for event in pygame.event.get():
-        if(event.type == pygame.QUIT):
-            running = False
                 
         
 pygame.quit()
